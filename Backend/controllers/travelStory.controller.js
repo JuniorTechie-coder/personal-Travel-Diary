7dbba2f6d40e1c9df1230016aa14d3b5d47db3b0
itsJuniorTechie <jtanmay787@gmail.com>
2026-03-03 21:00:23 +0530

    add travel story api route

---
+++ Backend/controllers/travelStory.controller.js
@@ -0,0 +1,38 @@
+import TravelStory from "../models/travelStory.model.js"
+import { errorHandlar } from "../utils/error.js"
+
+export const addTravelStory = async (req, res, next) => {
+const {title, story, visitedLoaction, imageUrl, visitedDate} = req.body
+
+
+
+// validate required field
+if (!title || !story || !visitedLoaction || !imageUrl || !visitedDate){
+    return next(errorHandlar(400, "All fields are required"))
+
+}
+
+const userId = req.user.id
+
+// convert visited date from milliseconds to Date Object
+const parsedVisitedDate = new Date(parseInt(visitedDate))
+
+try {
+    const travelStory = new TravelStory({
+        title,
+        story,
+        visitedLoaction,
+        userId,
+        imageUrl,
+        visitedDate: parsedVisitedDate,     
+    })
+    await travelStory.save()
+
+res.status(201).json({
+    story: travelStory,
+    message: "your Story is added successfully!",
+}) 
+} catch (error) {
+    next(error)
+}
+}
